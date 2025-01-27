ae29cd77dea1e342b7ce71305beb44d5769016a9
andrija14 <andrija.zecevic098@gmail.com>
2025-01-27 20:27:13 +0100

    backend8

--- routes/korisnici.js
+++ routes/korisnici.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 // Endpoint za dobavljanje svih korisnika
